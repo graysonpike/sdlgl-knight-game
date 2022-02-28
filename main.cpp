8c4c01cdf03d6ab86eafc0f7f45fd455ef7b4aeb
Grayson Pike <graysonpike@gmail.com>
2022-02-28 00:43:48 -0500

    Support new scene and audio

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include "sdlgl/graphics/graphics.h"
 #include <sdlgl/game/clock.h>
 #include <sdlgl/game/scene.h>
+#include <sdlgl/audio/audio.h>
 #include <sdlgl/input/inputs.h>
 #include <stdlib.h>
 #include <time.h>
 
 #include <sdlgl/ui/fps_display.h>
 #include <sdlgl/ui/entity_count.h>
+#include <sdlgl/game/context.h>
 
 #include "entities/knight.h"
 #include "ui/state_display.h"
@@ -17,22 +19,13 @@
 #define KEY_TOGGLE_DEBUG_VISUALS SDL_SCANCODE_GRAVE // Tilde
 
 
-struct Context {
-    Graphics *graphics;
-    Scene *scene;
-    Inputs *inputs;
-    Clock *clock;
-    bool *loop;
-};
-
-
-void game_loop(struct Context context) {
+void game_loop(Context context, Scene *scene) {
     context.inputs->update();
     context.clock->tick();
     context.graphics->clear_screen({255, 255, 255, 255});
     
-    context.scene->update(context.clock->get_delta());
-    context.scene->render();
+    scene->update(context.clock->get_delta());
+    scene->render();
 
     context.graphics->present_renderer(context.clock->get_delta());
 
@@ -52,36 +45,30 @@ int main() {
 
     srand(time(NULL));
 
-    struct Context context;
-
-    context.clock = new Clock();
-    context.inputs = new Inputs();
-
-    // Load a window
-    context.graphics = new Graphics(640, 480);
+    Context context(new Graphics(640, 480), new Audio(), new Inputs(), new Clock());
 
     // Load resources
     context.graphics->get_resources()->load_resources("resources.json");
 
     // Create and populate scene
-    context.scene = new Scene(context.inputs, context.graphics);
+    Scene *scene = new Scene(context.graphics, context.audio, context.inputs);
 
     // Game entities
     Knight *knight = new Knight(
-        context.scene, 100, 100, {255, 255, 255, 255});
-    context.scene->add_entity(knight);
+        scene, 100, 100, {255, 255, 255, 255});
+    scene->add_entity(knight);
 
     // UI entities
-    context.scene->add_entity(new FPS_Display(
-        context.scene, "base_text", {0, 0, 0, 255}));
-    context.scene->add_entity(new EntityCount(
-        context.scene, "base_text", {0, 0, 0, 255}));
-    context.scene->add_entity(new StateDisplay(
-        context.scene, "base_text", {0, 0, 0, 255}, knight));
+    scene->add_entity(new FPS_Display(
+        scene, "base_text", {0, 0, 0, 255}));
+    scene->add_entity(new EntityCount(
+        scene, "base_text", {0, 0, 0, 255}));
+    scene->add_entity(new StateDisplay(
+        scene, "base_text", {0, 0, 0, 255}, knight));
 
     *context.loop = true;
     while (*context.loop) {
-        game_loop(context);
+        game_loop(context, scene);
     }
 
     return 0;
